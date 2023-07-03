e3fcfcea97807aa6be5adfcaa86a049ca3361209
Sean O'Neil <40441698+saoneil@users.noreply.github.com>
2023-07-03 02:10:36 -0300

    updates ao 7.3.2023
    added dialog box for adding link and folder entry to save png qr more conveniently for user

--- signup_qr.py
+++ signup_qr.py
@@ -1,21 +1,55 @@
 import qrcode
+import tkinter as tk
+from tkinter import filedialog
 
-# Set the URL of the Google Form
-form_url = "https://forms.gle/45oqZ5orCxipq7iE9"
 
-qr = qrcode.QRCode(
-  version=1,
-  error_correction=qrcode.constants.ERROR_CORRECT_L,
-  box_size=10,
-  border=4,
-)
+def open_dialog():
+    global url_entry, folder_path_entry
 
-# Add the form URL to the QR code
-qr.add_data(form_url)
-qr.make(fit=True)
+    root = tk.Tk()
+    root.geometry("500x200")
 
-# Create an image of the QR code
-img = qr.make_image(fill_color="black", back_color="white")
+    # URL input
+    url_label = tk.Label(root, text="URL:")
+    url_label.pack()
+    url_entry = tk.Entry(root, width=60)
+    url_entry.pack()
 
-# Save the image of the QR code
-img.save("C:\\Users\\saone\\Desktop\\signup_qr.png")
+    # Folder path input
+    folder_path_label = tk.Label(root, text="Folder Path:")
+    folder_path_label.pack()
+    folder_path_entry = tk.Entry(root, width=60)
+    folder_path_entry.pack()
+
+    # Generate button
+    generate_button = tk.Button(root, text="Generate QR", command=generate_qr)
+    generate_button.pack(pady=(50,0))
+
+    root.mainloop()
+def generate_qr():
+    url = url_entry.get()
+    folder_path = folder_path_entry.get()
+
+    folder_path += "\signup_qr.png"
+
+    if url and folder_path:
+        qr = qrcode.QRCode(
+          version=1,
+          error_correction=qrcode.constants.ERROR_CORRECT_L,
+          box_size=10,
+          border=4,
+          )
+        
+        qr.add_data(url)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(folder_path)
+
+        print(f"Generating QR for URL: {url}")
+        print(f"Saving QR in folder: {folder_path}")
+    else:
+        print("URL or folder path is missing.")
+
+
+open_dialog()
